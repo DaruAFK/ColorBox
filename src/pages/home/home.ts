195acbddb072323cc8ce9f4b0843c2f7e697729a
Heli Perez <heli_perez@hotmail.com>
2018-02-01 02:03:03 -0400

    html y css

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,10 +9,26 @@ export class HomePage {
 
     boxes: any[];
     colors: string[];
+    colorSelected: string;
+    boxSelectedCount: number = 0;
+    maxBoxSelected: number = 3;
+    maxBoxes: number = 50;
 
     constructor(public navCtrl: NavController) {
         //aqui los colores
         this.colors = ['red', 'blue'];
+    }
+
+    selectBox(box) {
+        if(!box.selected) {
+            if(!this.colorSelected || box.color == this.colorSelected) {
+                this.boxSelectedCount ++;
+                box.selected = true;
+                if(this.boxSelectedCount == this.maxBoxSelected){
+                    //this.boxes.slice()
+                }
+            }
+        }
     }
 
 }
