8c9770bcff22540111d8362ae7315edca4f40c1f
Heli Perez <heli_perez@hotmail.com>
2018-02-01 01:32:55 -0400

    Cambio menor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    boxes: any[];
+    colors: string[];
 
-  }
+    constructor(public navCtrl: NavController) {
+        //aqui los colores
+        this.colors = ['red', 'blue'];
+    }
 
 }
