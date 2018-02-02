9bba0729c2333ddd6e3d9079cec084ed65c196e9
DaruAFK <daruafk@protonmail.com>
2018-02-02 15:06:39 -0400

    - TimerUpdate y Cambion en config.xml

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
 import { Observable } from 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'page-home',
@@ -16,14 +17,22 @@ export class HomePage {
     maxBoxes: number = 24;
     flag: boolean = true;
     score: number = 0;
+    timer: Subscription;
+    speed: number = 600;
+    accel: number = 50;
 
     constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
         //aqui los colores
         this.colors = ['#ff5050', '#ffd633', '#3366ff',
             '#33cc33', '#cc6699', '#996633', '#652996', '#8d8d8d'];
 
-        let timer = Observable.timer(1000, 500);
-        timer.subscribe(t => {
+        this.timerUpdate(this.speed);
+    }
+    timerUpdate(time){
+        if(this.timer){
+           this.timer.unsubscribe();
+        } 
+        this.timer = Observable.timer(time, time).subscribe(t => {
             if(this.flag){
                 if(this.boxes.length < this.maxBoxes) {
                     let number = Math.floor(Math.random() * this.colors.length);
@@ -35,7 +44,7 @@ export class HomePage {
                 }
             }            
         });
-    }
+    };
     showAlert(){
         let alert = this.alertCtrl.create({
           title: 'Game Over!',
@@ -48,6 +57,8 @@ export class HomePage {
                 this.boxSelectedCount = 0;
                 this.score = 0;
                 this.flag = true;
+                this.speed = 800;
+                this.accel = 50;
               }
           }],
           enableBackdropDismiss: false
@@ -65,6 +76,13 @@ export class HomePage {
                     this.boxSelectedCount = 0;
                     this.colorSelected = null;
                     this.score += 1;
+                    if(this.score % 20 == 0 && this.accel > 10){
+                        this.accel -= 10;
+                    }
+                    if(this.score % 10 == 0 && this.speed > 100){
+                        this.speed -= this.accel;
+                        this.timerUpdate(this.speed);
+                    }
                 }
             }
         }
